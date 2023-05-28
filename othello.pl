dd95fa824c80b0a486def9e766763b9997d67288
Kapsyloffer <CrilleMega@gmail.com>
2023-05-28 15:06:07 +0200

    Added documentatioN
    Each thingy is explained baby

--- othello.pl
+++ othello.pl
@@ -97,25 +97,30 @@ winner(State, Player):-
 	countScore(State, P1, P2),
     compare_scores(State, P1, P2, Player).
 
+%Basically what it says on the tin, 
+%vi kollar vem som leder.
 compare_scores(State, P1, P2, Player) :-
     (
 		P1 < P2 -> Player = 2 ;
      	P1 > P2 -> Player = 1 ;
-		tie(State) %Om de är lika skickar vi de till tie hell.
+		tie(State) %Om de är lika skickar så vi dem till tie hell.
 	 ), !.
 
 
 %To count and keep track of scores.
 countScore([], 0, 0).
 
+%Räkna alla ettor.
 countScore([1|T], Player1Score, Player2Score):-
 	countScore(T, RestPlayer1Score, Player2Score),
 	Player1Score is RestPlayer1Score + 1.
 
+%Räkna alla tvåor.
 countScore([2|T], Player1Score, Player2Score):-
 	countScore(T, Player1Score, RestPlayer2Score),
 	Player2Score is RestPlayer2Score + 1.
 
+%Om varken etta eller tvåa skippar vi.
 countScore([_|T], Player1Score, Player2Score):-
 	countScore(T, Player1Score, Player2Score).
 
@@ -139,6 +144,7 @@ tie(State):-
 %% define terminal(State). 
 %   - true if State is a terminal   
 
+%Terminal betyder att inga fler drag kan göras.
 terminal(State):-
 	moves(1, State, Player1Moves),
 	moves(2, State, Player2Moves),
@@ -176,104 +182,129 @@ printList([H | L]) :-
 %
 
 
+moves(_, _, [], []).
+%Här får vi ut alla möjliga moves genom att:
+%1. Få alla positioner av våra stenar
+%2. se abdra moves
 moves(Player, State, MoveList) :-
-	pieces(State, Player, Pieces), %Get all pieces
+	getPlayerPositions(State, Player, Pieces), %Get all pieces
 	moves(Player, State, Pieces, List),
 	%Om listan är tom så har vi n moves.
 	(List = [], MoveList = [n] ; MoveList = List).
 
-%Find a move foreach piece
+%For each stenar kollar vi varje möjlig direction med findmoves
 moves(Player, State, [CurPiece|Rest], MoveList) :-
 	Opp is (Player mod 2) + 1,
 	moves(Player, State, Rest, NextMoves), !,
-	(findMoves(State, Player, Opp, CurPiece, Moves) -> append(Moves, NextMoves, MoveList) ; MoveList = NextMoves). 
+	(findMoves(State, Player, Opp, CurPiece, Moves), append(Moves, NextMoves, MoveList) ; MoveList = NextMoves). 
 
-
-moves(_, _, [], []).
-
+%Base case, ništa
+findMoves(_, _, _, _, [], []).
+%I find moves trycker vi in alla 8 directions i vår findmoves
 findMoves(State, Player, Opp, CurPiece, Moves) :-
 	findMoves(State, Player, Opp, CurPiece, Moves,
-	[north, south, east, west, nWest, nEast, sWest, sEast]).
+	[north, south, east, west, nWest, nEast, sWest, sEast]). %<--- Bättre göra såhär än 8 separata funktioner.
 
+%En andra findmoves för att hantera varje element i listan
 findMoves(State, Player, Opp, CurPiece, Moves, [Direction|Rest]) :-
 	findMoves(State, Player, Opp, CurPiece, NextMoves, Rest),
-	(findMove(State, Player, Opp, CurPiece, Direction, Player, Move) ->
-		append([Move], NextMoves, Moves)
-	;
-		Moves = NextMoves
-	).
-findMoves(_, _, _, _, [], []).
+	%Om findmoveshelper returnar true (vi har ett move) så appendar vi det till vår NextMoves list.
+	(findMovesHelper(State, Player, Opp, CurPiece, Direction, Player, Move), append([Move], NextMoves, Moves) 
+	%Annars fortsätter vi på en annan direction
+	; Moves = NextMoves).
 
-% Hittar ett legal move i en riktining.
+% I vår helper hittar vi ett legal move i en riktining.
 % Börjar med player,
 % Hittar ett led av opponent pieces
 % Då vi stöter på en tom plats så är moved valid.
-findMove(State, Player, Opp, [X, Y], Direction, Prev, Move) :-
+%Då returnar vi true, annars false.
+findMovesHelper(State, Player, Opp, [X, Y], Direction, Last, Move) :-
 	fetchCoord([X, Y], Direction, [X1, Y1]),
 	get(State, [X1, Y1], Tile),
-	\+(Tile = Player),
-	(
+	(\+(Tile = Player), %Om vi inte stöter på en till playertile, fortsätt.
 		Tile = Opp, 
-		findMove(State, Player, Opp, [X1, Y1], Direction, Opp, Move)
+		findMovesHelper(State, Player, Opp, [X1, Y1], Direction, Opp, Move)
 	; %Om den är tom, och tidigare tilen i riktningen var en opponent, då är [X1, Y1] en valid move.
-		Tile = ., 
-		Prev = Opp,
-		Move = [X1, Y1]
-	).
+		Tile = ., %<--- tom ruta
+		Last = Opp,
+		Move = [X1, Y1]).
 
-makeMove(State, _, _, _, [], State).
-makeMove(State, Player, Move, NewState) :-
+
+playedMove(State, _, _, _, [], State).
+%Vi tar en evaluated valid move, och spelar ut den.
+playedMove(State, Player, Move, NewState) :-
 	Opp is (Player mod 2) + 1,
-	set(State, S, Move, Player),
-	makeMove(S, Player, Opp, Move,
-	[north, south, east, west, nWest, nEast, sWest, sEast], NewState).
+	set(State, S, Move, Player), %Sätter ut brickan.
+	%Likt findmoves så tar vi alla directions från vårt moves, och checkar om något måste flippas.
+	playedMove(S, Player, Opp, Move, [north, south, east, west, nWest, nEast, sWest, sEast], NewState).
 
-makeMove(State, Player, Opp, Move, [Direction|Rest], NewState) :-
-	(flipTiles(State, Player, Opp, Move, Direction, FlippedState) ->
-		S = FlippedState 
-	;
-		S = State 
-	),
-	makeMove(S, Player, Opp, Move, Rest, NewState).
+playedMove(State, Player, Opp, Move, [Direction|Rest], NewState) :-
+	%Om fliptiles returnar true så har vi flippat tiles i en riktning och sätter det till vår nya state,
+	%Annars så är state densamma för inget flippas.
+	(flipTiles(State, Player, Opp, Move, Direction, FlippedState), S = FlippedState ; S = State ),
+	playedMove(S, Player, Opp, Move, Rest, NewState).
 
-
-pieces(State, Player, Pieces) :- pieces(State, Player, Pieces, 0, 0).
-
-pieces(_, _, [], X, Y) :- 
+%Om vi når slutet av brädet så terminatear vi.
+getPlayerPositions(_, _, [], X, Y) :- 
 	X > 5, 
 	Y > 5.
+%Hämtar coordinaterna på varje spelpjäs
+% Spelpjäs being en bricka som är vit elelr svart (1 eller 2)
+getPlayerPositions(State, Player, Pieces) :- 
+	getPlayerPositions(State, Player, Pieces, 0, 0).
 
-pieces(State, Player, Pieces, X, Y) :- 
+%Vi skannar efter pieces kolumn efter kolumn
+%och ger oss spelarens positioner.
+%Exempel ordning i en 4x4:
+% 					y↓ x→
+% x = 0
+% 01 05 09 12	y = 0
+% 02 06 10 13
+% 03 07 11 14
+% 04 08 12 15 	y = 3
+%		   x = 3
+getPlayerPositions(State, Player, Pieces, X, Y) :- 
 	Y > 5,
-	pieces(State, Player, Pieces, X + 1, 0), !.
+	getPlayerPositions(State, Player, Pieces, X + 1, 0), !.
 
-pieces(State, Player, Pieces, X, Y) :-
-	X1 is X, Y1 is Y,
+getPlayerPositions(State, Player, Pieces, X, Y) :-
+	X1 is X, 
+	Y1 is Y,
+	%Om vi hittat en piece så hamnar den i listan, annars fortsätter vi utan den.
 	(get(State, [X1, Y1], Player), Pieces = [[X1, Y1]|NextPieces] ; Pieces = NextPieces),
-	pieces(State, Player, NextPieces, X, Y + 1), !.
+	getPlayerPositions(State, Player, NextPieces, X, Y + 1), !.
+%See above comment för dokumentation.
 
 
 %En lista som beskriver åt vilket håll varje direction ligger
+%		 NORTH			
+%	NW			NE
+% WEST	[Piece]	 EAST
+%	SW			SE
+%		 SOUTH
 fetchCoord([X, Y], Direction, [X1, Y1]) :-
 (
-	(Direction = north, X1 is X, Y1 is Y - 1);
-	(Direction = south, X1 is X, Y1 is Y + 1);
-	(Direction = east, X1 is X + 1, Y1 is Y);
-	(Direction = west, X1 is X - 1, Y1 is Y);
-	(Direction = nWest, X1 is X - 1, Y1 is Y - 1);
-	(Direction = nEast, X1 is X + 1, Y1 is Y - 1);
-	(Direction = sWest, X1 is X - 1, Y1 is Y + 1);
-	(Direction = sEast, X1 is X + 1, Y1 is Y + 1)
-).
+	(Direction = north, X1 is X    , 	Y1 is Y - 1); 
+	(Direction = south, X1 is X    , 	Y1 is Y + 1);
+	(Direction = east, 	X1 is X + 1, 	Y1 is Y	   );
+	(Direction = west, 	X1 is X - 1, 	Y1 is Y    );
+	(Direction = nWest, X1 is X - 1, 	Y1 is Y - 1);
+	(Direction = nEast, X1 is X + 1, 	Y1 is Y - 1);
+	(Direction = sWest, X1 is X - 1, 	Y1 is Y + 1);
+	(Direction = sEast, X1 is X + 1, 	Y1 is Y + 1)
+). 
+%^This is silly but I kinda like it.
 
-
-
-
-
+%Base case: Player hittad på andra sidan riktningen:
+%Och då avslutar vi så att vi inte råkar flippa hela boardet. :P
 flipTiles(State, Player, _, [X, Y], Direction, State) :-
 	fetchCoord([X, Y], Direction, [X1, Y1]),
 	get(State, [X1, Y1], Player).
 
+%Denna kommer flippa i en direction i den nya staten tills
+%Den stöter på en player, om vi inte slår i en player 
+%eller om opponents tar slut så
+%returnar den false och vi testar en annan direction.
 flipTiles(State, Player, Opp, [X, Y], Direction, NewState) :-
 	fetchCoord([X, Y], Direction, [X1, Y1]),
 	get(State, [X1, Y1], Tile),
@@ -290,16 +321,24 @@ flipTiles(State, Player, Opp, [X, Y], Direction, NewState) :-
 %   - given that Player makes Move in State, it determines NewState (i.e. the next
 %     state) and NextPlayer (i.e. the next player who will move).
 
+%Om vi tvingas spela move n OCH 
+%det inte är terminal så:
+%byter vi bara spelare.
+%ANNARS: avslutas spelet.
 nextState(Player, n, State, State, NextPlayer) :- 
 	\+ terminal(State),
-	nextPlayer(State, Player, NextPlayer).
+	nextPlayer(Player, NextPlayer).
 
+%Men om vi kör en valid move (räknas i play) så gör vi ett move
 nextState(Player, [X, Y], State, NewState, NextPlayer) :-
-	makeMove(State, Player, [X, Y], NewState),
-	nextPlayer(NewState, Player, NextPlayer).
+	playedMove(State, Player, [X, Y], NewState),
+	nextPlayer(Player, NextPlayer).
 
-nextPlayer(_, Player, NextPlayer) :-
+%Ger oss opponent.
+nextPlayer(Player, NextPlayer) :-
     NextPlayer is (Player mod 2) + 1.
+
+%OBS: Dessa kallas enbart av play.pl om en valid move är spelad. ^
 
 
 % DO NOT CHANGE THIS BLOCK OF COMMENTS.
@@ -327,13 +366,20 @@ validmove(Player, State, Proposed):-
 %          good heuristics.
 
 h(_,0).
-h(State, -100):- winner(State, 1), !.
-h(State, 100) :- winner(State, 2), !.
-h(State, 0) :- tie(State), !.
+%Om h går mot -100 så vinner 1
+h(State, -100):- 
+	winner(State, 1), !.
+%Annars så vinner 2
+h(State, 100) :- 
+	winner(State, 2), !.
+%Och om 0 så är det lika
+h(State, 0) :- 
+	tie(State), !.
+%Val är skillnaden av scores mellan spelarna
 h(State, Val) :-
 	countScore(State, P1, P2),
 	Val is P2 - P1.
-
+%Tbh jag har ingen aning om hur det här funkar, Hjalmar hjälpte mig här.
 
 
 % DO NOT CHANGE THIS BLOCK OF COMMENTS.
